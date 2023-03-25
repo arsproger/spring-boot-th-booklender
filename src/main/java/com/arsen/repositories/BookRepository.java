12917ad3b0ca282814a78d5e61019475b3afa3ee
Atai <abektenaliev@inbox.ru>
2023-03-26 00:57:47 +0600

    add methods in User Book repository

--- src/main/java/com/arsen/repositories/BookRepository.java
+++ src/main/java/com/arsen/repositories/BookRepository.java
@@ -1,10 +1,16 @@
 package com.arsen.repositories;
 
 import com.arsen.models.Book;
+import com.arsen.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query(value = "select * from books where books.user_id = :users.id", nativeQuery = true)
+    abstract List<Book> getBookByUser(User user);
 }
 
--- src/main/java/com/arsen/repositories/UserRepository.java
+++ src/main/java/com/arsen/repositories/UserRepository.java
@@ -1,12 +1,18 @@
 package com.arsen.repositories;
 
+import com.arsen.models.Book;
 import com.arsen.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * from users where users.id = :books.user", nativeQuery = true)
+    abstract List<User> getUserByBook(Book book);
 }
