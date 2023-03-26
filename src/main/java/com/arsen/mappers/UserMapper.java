6ac65c8b9e11b720f1cea0d6a8dd20d447574241
Arsen <arsproger@gmail.com>
2023-03-26 17:00:42 +0600

    clear comments

--- src/main/java/com/arsen/mappers/UserMapper.java
+++ src/main/java/com/arsen/mappers/UserMapper.java
@@ -1,5 +1,14 @@
 package com.arsen.mappers;
 
-public class UserMapper {
+import com.arsen.dto.UserDTO;
+import com.arsen.models.User;
+import org.mapstruct.Mapper;
 
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDTO userToUserDTO(User user);
+    User userDTOtoUser(UserDTO userDTO);
+    List<UserDTO> userListToUserDTOList(List<User> userList);
 }
--- src/main/java/com/arsen/services/BookService.java
+++ src/main/java/com/arsen/services/BookService.java
@@ -1,19 +1,26 @@
 package com.arsen.services;
 
+import com.arsen.enums.BookStatus;
 import com.arsen.models.Book;
+import com.arsen.models.Record;
+import com.arsen.models.User;
 import com.arsen.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final UserService userService;
 
     @Autowired
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, UserService userService) {
         this.bookRepository = bookRepository;
+        this.userService = userService;
     }
 
     public List<Book> getAllBooks() {
@@ -23,6 +30,16 @@ public class BookService {
     public Book getBookById(Long id) {
         return bookRepository.findById(id).orElse(null);
     }
+    public Book updateBook(Long id, Book book){
+        Book updatedBook = getBookById(id);
+        updatedBook.setName(book.getName());
+        updatedBook.setAuthor(book.getAuthor());
+        updatedBook.setStatus(book.getStatus());
+        updatedBook.setRecords(book.getRecords());
+        updatedBook.setImage(book.getImage());
+        updatedBook.setUser(book.getUser());
+        return bookRepository.save(updatedBook);
+    }
 
     public void saveBook(Book book) {
         bookRepository.save(book);
@@ -30,6 +47,32 @@ public class BookService {
 
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
+    }
+
+    public void lendBook(Long userId, Long bookId) {
+        Book book = getBookById(bookId);
+        User user = userService.getUserById(userId);
+        book.setStatus(BookStatus.BORROWED);
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        user.setCurrentBooks(books);
+        Record record = new Record();
+        record.setLoanDate(LocalDateTime.now());
+        record.setBook(book);
+        record.setUser(user);
+    }
+    public void returnBook(Long userId, Long bookId) {
+        Book book = getBookById(bookId);
+        User user = userService.getUserById(userId);
+        book.setStatus(BookStatus.AVAILABLE);
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        user.setPastBooks(books);
+
+        Record record = new Record();
+        record.setReturnDate(LocalDateTime.now());
+        record.setBook(book);
+        record.setUser(user);
     }
 
 }
