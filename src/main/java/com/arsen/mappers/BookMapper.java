25199199f8bfb2033247cb21651e8ca04e803e1c
Arsen <arsproger@gmail.com>
2023-03-28 16:56:05 +0600

    add modelmapper

--- src/main/java/com/arsen/mappers/BookMapper.java
+++ src/main/java/com/arsen/mappers/BookMapper.java
@@ -2,15 +2,24 @@ package com.arsen.mappers;
 
 import com.arsen.dto.BookDTO;
 import com.arsen.models.Book;
-import org.mapstruct.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
-@Mapper(componentModel = "spring")
-public interface BookMapper {
-    BookDTO bookToBookDTO(Book book);
-    Book bookDTOtoBook(BookDTO bookDTO);
-    List<BookDTO> bookListToBookDTOList(List<Book> bookList);
+public class BookMapper {
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public BookMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public BookDTO convertToDTO(Book book) {
+        return modelMapper.map(book, BookDTO.class);
+    }
+
+    public Book convertToEntity(BookDTO bookDTO) {
+        return modelMapper.map(bookDTO, Book.class);
+    }
 }
