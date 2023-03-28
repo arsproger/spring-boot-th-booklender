c8d569cfbb8bfd253b087c6de9c32240ca058e90
Arsen <arsproger@gmail.com>
2023-03-28 12:06:07 +0600

    add styles and js

--- src/main/java/com/arsen/mappers/RecordMapper.java
+++ src/main/java/com/arsen/mappers/RecordMapper.java
@@ -3,9 +3,11 @@ package com.arsen.mappers;
 import com.arsen.dto.RecordDTO;
 import com.arsen.models.Record;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface RecordMapper {
     RecordDTO recordToRecordDTO(Record record);
